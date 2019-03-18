2958d6e6bef6d15cba385b254f47db75d055be5d
Kristen Coates <coates.kristen@gmail.com>
2019-03-18 12:34:01 -0600

    Adding Mocha/Chai Test Model

---
+++ Models/models.js
@@ -0,0 +1,21 @@
+(function() {
+    "use strict";
+
+    // Test Setup
+    mocha.setup("bdd");
+    const { expect } = chai;
+
+    //Test Suite
+    describe("A function", function() {
+        it("should fulfill this requirement", function() {
+            
+            /*enter code that makes the test pass here*/
+
+            /*enter assertion here*/
+            
+        });
+    });
+
+
+    mocha.run();
+})();

8909bd9676f434870be22c21d03696672cab02ff
Kristen Coates <coates.kristen@gmail.com>
2019-03-30 19:02:45 -0600

    Adding updated sum function requirements and starter code for lightning talk code along models.js

---
+++ tdd-sum-function/models.js
@@ -0,0 +1,19 @@
+(function() {
+    "use strict";
+
+    // Test Setup
+    mocha.setup("bdd");
+    const { assert } = chai;
+
+    //Test Suite
+    describe("A function that adds numbers", function() {
+        it("should.... ", function() {
+
+        });
+        it("should.... ", function() {
+
+        });
+    });
+
+    mocha.run();
+})();
